d60ead93c3ecacde19d537afcbe886e0cdea17ca
fany0r <29101894+fany0r@users.noreply.github.com>
2024-11-17 01:05:30 +0800

    新增通过 Actions 编译

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # Git for Windows 汉化
 
-## 用法
+## 2024-11-17 新增通过 Actions 编译
+- 可选：修改 build.yml 中的 `GIT_VERSION` 变量，指定需要汉化的 Git 版本。
+- 手动执行 Actions，然后下载生成的压缩包，把里面的东西（mingw64）解压到 Git 安装目录即可。
+
+## 本地编译
+
+以下步骤是原文作者写的步骤 [@toyobayashi](https://github.com/toyobayashi/git-zh)
 
 在 WSL 或 git bash 中
 
@@ -34,3 +40,7 @@ curl -L -O "https://hub.fastgit.org/git-for-windows/git/archive/refs/tags/$tarfi
 /mingw64/share/git-gui/lib/msgs/zh_cn.msg
 /mingw64/share/gitk/lib/msgs/zh_cn.msg
 ```
+
+## 汉化预览
+
+![](Snipaste_2024-11-17.png)
--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "GIT_VERSION: $1"
 
 if [ "x$1" == "x" ]; then
   echo "Missing git version"
@@ -10,13 +11,12 @@ tarfile="v$gitver.tar.gz"
 
 if [ ! -f "$tarfile" ]; then
   curl -L -O "https://github.com/git-for-windows/git/archive/refs/tags/$tarfile"
-  # curl -L -O "https://hub.fastgit.org/git-for-windows/git/archive/refs/tags/$tarfile"
+  # https://github.com/git-for-windows/git/archive/refs/tags/v2.47.0.windows.2.tar.gz
 fi
 
 tar xf "$tarfile"
 
 dir="git-$gitver"
-
 outdir="build"
 
 modir="$outdir/mingw64/share/locale/zh_CN/LC_MESSAGES"
@@ -26,6 +26,7 @@ gitkdir="$outdir/mingw64/share/gitk/lib/msgs"
 mkdir -p $modir
 mkdir -p $guidir
 mkdir -p $gitkdir
+
 msgfmt -o "$modir/git.mo" ./$dir/po/zh_CN.po
 msgfmt --tcl -l zh_CN -d "$guidir" ./$dir/git-gui/po/zh_cn.po
 msgfmt --tcl -l zh_CN -d "$gitkdir" ./$dir/gitk-git/po/zh_cn.po
@@ -33,26 +34,45 @@ msgfmt --tcl -l zh_CN -d "$gitkdir" ./$dir/gitk-git/po/zh_cn.po
 zipname="build-$gitver.zip"
 
 if [ -d "$outdir" ]; then
+  pwd & ls -l
   rm -rf "$zipname"
   
-  type zip >/dev/null 2>&1
-  if [ "x$?" == "x0" ]; then
+  if command -v zip &>/dev/null; then
     cd "$outdir"
     zip -r -y "../$zipname" .
     cd ..
   else
-    powershell.exe -nologo -noprofile -command \
-      '& { param([String]$sourceDirectoryName, [String]$destinationArchiveFileName, [Boolean]$includeBaseDirectory); Add-Type -A "System.IO.Compression.FileSystem"; Add-Type -A "System.Text.Encoding"; [IO.Compression.ZipFile]::CreateFromDirectory($sourceDirectoryName, $destinationArchiveFileName, [IO.Compression.CompressionLevel]::Fastest, $includeBaseDirectory, [System.Text.Encoding]::UTF8); exit !$?;}' \
-      -sourceDirectoryName "\"$outdir\"" \
-      -destinationArchiveFileName "$zipname" \
-      -includeBaseDirectory '$false'
-    if [ $? -ne 0 ]; then
-      echo "Zip failed"
-      exit $?
+    # 如果 zip 不可用，尝试使用 PowerShell Core
+    if command -v pwsh &>/dev/null; then
+      pwsh -Command \
+        '& { param([String]$sourceDirectoryName, [String]$destinationArchiveFileName, [Boolean]$includeBaseDirectory); Add-Type -A "System.IO.Compression.FileSystem"; Add-Type -A "System.Text.Encoding"; [IO.Compression.ZipFile]::CreateFromDirectory($sourceDirectoryName, $destinationArchiveFileName, [IO.Compression.CompressionLevel]::Fastest, $includeBaseDirectory, [System.Text.Encoding]::UTF8); exit !$?;}' \
+        -sourceDirectoryName "$outdir" \
+        -destinationArchiveFileName "$zipname" \
+        -includeBaseDirectory '$false'
+      if [ $? -ne 0 ]; then
+        echo "PowerShell zip failed"
+        exit $?
+      fi
+    else
+      echo "Neither zip nor PowerShell is available for compression"
+      exit 1
     fi
   fi
 fi
 
+if [ -f "$zipname" ]; then
+  echo "Zip file created: $zipname"
+  echo "ZIP_FILE=$zipname" >> $GITHUB_ENV
+  pwd
+  ls -l
+else
+  echo "Error: Zip file not created: $zipname"
+  ls -R
+  exit 1
+fi
+
+
+# 清理临时文件
 rm -rf "$outdir"
 rm -rf "$dir"
-# rm -rf "$tarfile"
+# rm -rf "$tarfile" # 可选：保留 tar 文件以便重复使用
